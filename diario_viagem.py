1a4c8400d03d8722b2f19da767132de6af707970
ederson <edersonsgoncalves@gmail.com>
2025-07-13 21:15:19 -0300

    feat: modelar __str__ classe RegistroViagem

--- diario_viagem.py
+++ diario_viagem.py
@@ -1,3 +1,15 @@
 class RegistroViagem:
-    def __init__(self):
-        pass
+    def __init__(self, destino:str, data_viagem,atividades:list):
+        self.destino = destino
+        self.data_viagem = data_viagem
+        self.atividades = atividades
+    
+    def __str__(self):
+        lista_atividades = ""
+        for atividade in self.atividades:
+            lista_atividades += "* " + atividade + "\n"
+
+        return f"""
+\033[1m[{self.destino}]\033[0m - {self.data_viagem}
+{lista_atividades}
+"""
