4e1ea56f35211571ee81d89733ec93546c1b95bf
ederson <edersonsgoncalves@gmail.com>
2025-07-13 21:32:26 -0300

    feat: implementar método qtd_atividades() e executá-lo

--- diario_viagem.py
+++ diario_viagem.py
@@ -11,11 +11,16 @@ class RegistroViagem:
 
         return f"""
 \033[1m[{self.destino}]\033[0m - {self.data_viagem}
-{lista_atividades}
-"""
+{lista_atividades[:-2]}"""
     def adicionar_atividade(self, atividade:str):
         self.atividades.append(atividade)
 
+    def qtd_atividades(self):
+        contador = 0
+        for atividade in self.atividades:
+            contador += 1
+        
+        return f"A viagem para {self.destino} tem {contador} atividades."
 
 paris = RegistroViagem(destino="Paris", data_viagem="15/04/2023")
 paris.adicionar_atividade("Torre Eifel")
@@ -23,3 +28,4 @@ paris.adicionar_atividade("Euro Disney")
 paris.adicionar_atividade("Louvre")
 
 print (paris)
+print (paris.qtd_atividades())
